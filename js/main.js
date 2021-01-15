1956cc3bad6b87e12b2a9dd5c4d6e284f16eeb1c
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2021-01-15 18:17:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 let wineMenu = document.querySelector('.wine-menu');
 let wineMenuDropdown = document.querySelector('.wine-menu_dropdown');
+let burgerOpen = document.querySelector('#burger-open');
+let headerMenu = document.querySelector('.header-dropdown');
+
 
 wineMenu.addEventListener('click', function () {
     wineMenuDropdown.classList.toggle('active');
+});
+
+burgerOpen.addEventListener('click', function () {
+    headerMenu.classList.toggle('active');
 });
